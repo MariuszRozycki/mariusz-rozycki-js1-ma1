8a80f00a2441d894f36e7894aa5e3769428a4e43
MariuszRozycki <mariansc83@icloud.com>
2022-04-08 13:40:14 +0200

    Add answer to question 3

--- js/script.js
+++ js/script.js
@@ -12,3 +12,8 @@ console.log(cat.complain());
 // Question 2
 const heading = document.querySelector("h3");
 heading.innerHTML = "Updated heading";
+
+
+
+// Question 3
+heading.style.fontSize = "2em"; 

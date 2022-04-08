697f1a206deda4509365f925e3ab94a4dfb39884
MariuszRozycki <mariansc83@icloud.com>
2022-04-08 13:34:54 +0200

    Add answer to question 1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-const cats = [
-  {
-    name: "Blob",
-    age: 10
-  },
-  {
-    name: "Harold",
-  },
-  {
-    name: "Blurt",
-    age: 21
+// Question 1
+const cat = {
+  complain: function () {
+    console.log("Meow!");
   }
-];
+};
+
+console.log(cat.complain());
+

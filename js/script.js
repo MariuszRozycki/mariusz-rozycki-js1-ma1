765a96055cc74f9ff8c5e8a5d4316ec9cbb73118
MariuszRozycki <mariansc83@icloud.com>
2022-04-08 14:42:19 +0200

    Add answer to question 8

--- js/script.js
+++ js/script.js
@@ -56,8 +56,39 @@ const cats = [
 
 function showCatsNames(list) {
   for (let cat of list) {
-    console.log(cat.name);
+    console.log("Cat name:", cat.name);
   }
 }
 
 showCatsNames(cats);
+
+
+// Question 8 - I'm using here the same array cats as in question 7
+function createCats(cats) {
+
+  let html = "";
+
+  for (let cat of cats) {
+    let catName = cat.name;
+    let catAge = "Age unknown";
+
+
+    if (cat.age) {
+      catAge = cat.age;
+    }
+
+    html += `<div>
+              <h5>Cat name: ${catName}</h5>
+              <p>Cat age: ${catAge}</p>
+            </div>`
+  }
+
+  return html;
+
+}
+
+const html = createCats(cats);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = html;
+
